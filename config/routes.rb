99b29ec8527be3f423f959b69407a1eb8bee58f5
codedit334 <oussama7elabdioui@gmail.com>
2023-07-11 09:44:07 +0100

    Create API EndPoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/random'
+      get '/random_greeting', to: 'greetings#random'
+    end
+  end
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
